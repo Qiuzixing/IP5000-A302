300dae306ba386fb50248d1cf56dc6fa8227be1c
Sid Shann <sid.shann@grandbeing.com>
2021-09-28 15:56:16 +0800

    [Feature] 增加恢复出厂同时恢复EDID的功能
    Change-Id: I76a6addf434eeac4c477d330b7061585ecdd652c

--- filesystem/basic_kvme/usr/local/bin/reset_to_default.sh
+++ filesystem/basic_kvme/usr/local/bin/reset_to_default.sh
@@ -5,5 +5,11 @@ echo "Reset to factory default..."
 ./astparam flush
 ./astparam save
 
+# reset Encoder EDID to default HDMI
+if [ -e /sys/devices/platform/videoip/eeprom_content ]; then
+    cat /sys/devices/platform/display/default_edid_hdmi > /sys/devices/platform/videoip/eeprom_content
+fi
+
 rm /data/configs/kds-7/ -rf
+sync
 echo "done"
