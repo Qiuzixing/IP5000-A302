b5a971a0dbc9bf9a6933a8a10d0b35a3449dd0ee
longao <longao@proitav.com>
2021-12-24 16:44:54 +0800

    [Improve]配合osd修改SetOsdDisplay处理
    Change-Id: I619f07e01bbe929720cc9cc16e09c63638c88bc1

--- application/ast_app/applet/p3kmoudle/p3klib/funcexcute.cpp
+++ application/ast_app/applet/p3kmoudle/p3klib/funcexcute.cpp
@@ -1093,6 +1093,11 @@ int EX_SetOsdDisplay(int    mode )
 {
 	char buf[64] = "";
 	State_E osd_mode = OFF;
+    
+    char SetOsdDisplay[BUFSIZE] = {0};
+	sprintf(SetOsdDisplay,"SET_OSD_DISPALY  %d",mode);
+	int ret = sendCmdtoGUI(SetOsdDisplay);
+    #if 0
 	if(mode == 0)
 		osd_mode = OFF;
 	else if((mode == 1)||(mode == 2))
@@ -1111,6 +1116,7 @@ int EX_SetOsdDisplay(int    mode )
 		mysystem("echo Hotkey3 > /var/info_local", buf, sizeof(buf));
 	}
 	//osd mode {0	¨C off,1	¨C on,2	¨C display now,}
+    #endif
 	return 0;
 }
 int EX_GetOsdDisplay(void)
